b26ca098e64ecd779637037900f9d53f429562fe
Hasdrubal <ronaldolsthoorn1@gmail.com>
2022-12-19 17:48:55 +0100

    before github

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,173 @@
+use std::borrow::BorrowMut;
+use std::cell::{Ref, RefCell, RefMut};
+use std::rc::{Rc, Weak};
 use std::fs::File;
-use std::io::{prelude::*, BufReader};
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::io::{BufRead, BufReader};
+use std::ops::Deref;
+use std::os::linux::raw::stat;
 
 
-fn main() {
-    let reader = BufReader::new(File::open("input.txt").unwrap());
+enum UnixFile{
+    FILE(UFile),
+    FOLDER(UFolder)
+}
 
-    let mut emptyLineNumber = 0;
+struct UFile {
+    name: String,
+    parent: Weak<RefCell<UnixFile>>,
+    size: usize,
+}
 
-    for (i, line) in reader.lines().enumerate() {
-        if line.unwrap().is_empty() {
-            emptyLineNumber = i;
+struct UFolder {
+    name: String,
+    parent: Weak<RefCell<UnixFile>>,
+    files: Vec<Rc<RefCell<UnixFile>>>,
+}
+
+impl UFile {
+    pub fn size(&self) -> usize {
+        self.size
+    }
+}
+
+impl UFolder {
+    pub fn size(&self) -> usize {
+        let mut s: usize = 0;
+        for uFile in &self.files {
+            s +=  uFile.borrow().size();
+        }
+        s
+    }
+}
+
+impl UnixFile {
+    pub fn size(&self) -> usize {
+        match &self {
+            UnixFile::FILE(file) => file.size(),
+            UnixFile::FOLDER(folder) => folder.size()
         }
     }
 
-    let reader = BufReader::new(File::open("input.txt").unwrap());
-    let lines: Vec<String>= reader.lines().map(|l| l.unwrap()).collect::<Vec<String>>();
-    let line: &String = &lines[0];
+    pub fn from_string(ls_string: String) -> UnixFile {
 
-    let mut deque = VecDeque::new();
+        let splits = ls_string.split_whitespace();
+        let words: Vec<&str> = splits.collect::<Vec<&str>>();
 
-    for (i, new_character) in line.chars().enumerate() {
-
-        deque.push_front(new_character);
-
-        if deque.len() > 14 {
-            deque.pop_back();
+        if words[0] == "dir".to_string() {
+            UnixFile::FOLDER(UFolder{
+                name: words[1].to_owned(),
+                files: vec![],
+                parent: Weak::new()
+            })
+        } else{
+            UnixFile::FILE(UFile{
+                name: words[1].to_owned(),
+                size: words[0].parse::<usize>().unwrap(),
+                parent: Weak::new()
+            })
         }
+    }
+}
 
-        if deque.len() == 14 {
 
-            let mut unique = true;
+struct Filter{
+    total_size: usize
+}
 
-            for j in 0..14 {
-                for k in j+1..14 {
-                    unique &= deque[j] != deque[k];
-                    if !unique {
-                        println!("j {} k {} deque[j] {} deque[k] {}", j, k, deque[j], deque[k]);
-                        break;
+const MAX_SIZE: usize = 100000;
+
+impl Filter{
+
+    fn walk(mut self, root: Rc<RefCell<UnixFile>>){
+
+        let mut cache_children = Vec::new();
+
+        while !cache_children.is_empty() {
+
+            let mut popped_child:Rc<RefCell<UnixFile>> = cache_children.pop().unwrap();
+
+            {
+                let size = (*popped_child).borrow().size();
+
+                if size < MAX_SIZE {
+                    self.total_size += size;
+                }
+            }
+
+            match &*(popped_child.borrow()){
+                UnixFile::FILE(file) => {}
+                UnixFile::FOLDER(folder) => {
+                    for child in &folder.files{
+                        cache_children.push(child.clone());
                     }
                 }
+            };
+        }
+    }
+}
 
-                if !unique {
-                    break;
-                }
+enum Command {
+    CD(String),
+    LS
+}
+
+impl Command {
+    fn from_string(command_string: String) -> Command {
+        let mut words: Vec<&str> = command_string.split_whitespace().collect::<Vec<&str>>();
+        if words.len() > 1 {
+            return Command::CD(words[0].to_owned());
+        }
+        return Command::LS;
+    }
+}
+
+fn main() {
+
+    let reader = BufReader::new(File::open("input.txt").unwrap());
+
+    let root = Rc::new(RefCell::new(
+        UnixFile::FOLDER(
+            UFolder{
+                name: String::from("root"),
+                parent: Weak::new(),
+                files: vec![],
+            })));
+
+    let mut current = root.clone();
+    let mut state_idle = true;
+    let mut cache_files: Vec<Rc<RefCell<UnixFile>>> = Vec::new();
+
+    for line in reader.lines().map(|l| l.unwrap()){
+
+        if !state_idle && line.chars().nth(0).unwrap() == '$' {
+
+            let mut f = &*(*current).borrow_mut();
+
+            match f {
+                UnixFile::FOLDER(mut folder) => {
+                    folder.files.append(&mut cache_files);
+                },
+                UnixFile::FILE(file) => {}
             }
 
-            if unique {
-                let mut print_out = format!("found at i {}, ", i);
 
-                for c in deque {
-                    print_out.push(c);
+            state_idle = false;
+        }
+
+        if state_idle {
+            let command = Command::from_string(line);
+            match  command {
+                Command::CD(String) => {
+                    
+                },
+                Command::LS => {
+                    state_idle = false;
                 }
-                println!("{}", print_out);
-                break;
             }
+        } else{
+            cache_files.push(Rc::new(RefCell::new(
+                UnixFile::from_string(line)
+            )));
         }
     }
 }
